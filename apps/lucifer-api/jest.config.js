1e603e1297bf37018954d3e5a9c95b679eed6e22
Julien Capellari <julien@capellari.net>
2020-11-14 00:57:32 +0100

    add: generate lucifer-api

---
+++ apps/lucifer-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'lucifer-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/lucifer-api',
+};
