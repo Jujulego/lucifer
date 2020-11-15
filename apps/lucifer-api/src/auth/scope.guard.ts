c2972bc2e28a3db2fadff70e521520dcd657f1b0
Julien Capellari <julien@capellari.net>
2020-11-15 17:55:31 +0100

    add(lucifer-api): scope guard

---
+++ apps/lucifer-api/src/auth/scope.guard.ts
@@ -0,0 +1,48 @@
+import { CanActivate, CustomDecorator, ExecutionContext, Injectable, SetMetadata } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { User } from './user.model';
+
+// Types
+export type AllowIfCallback<R> = (req: R, user: User) => boolean;
+
+// Symbols
+const METADATA_KEYS = {
+  scopes: Symbol('scope:scopes'),
+  allow:  Symbol('scope:allow'),
+}
+
+// Decorators
+export function Scopes(...scopes: string[]): CustomDecorator<symbol> {
+  return SetMetadata(METADATA_KEYS.scopes, scopes);
+}
+
+export function AllowIf<R = unknown>(cb: AllowIfCallback<R>): CustomDecorator<symbol> {
+  return SetMetadata(METADATA_KEYS.allow, cb);
+}
+
+// Guard
+@Injectable()
+export class ScopeGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector
+  ) {}
+
+  // Methods
+  canActivate(ctx: ExecutionContext): boolean {
+    // Get metadata
+    const scopes = this.reflector.get<string[]>(METADATA_KEYS.scopes, ctx.getHandler());
+    const allow = this.reflector.get<AllowIfCallback<unknown>>(METADATA_KEYS.allow, ctx.getHandler());
+
+    if (!scopes || scopes.length === 0) return true;
+
+    // Get token
+    const request = ctx.switchToHttp().getRequest();
+    const token = request.user as User;
+    if (!token || !token.permissions) return false;
+
+    // Match
+    if (allow && allow(request, token)) return true;
+    return scopes.every(scope => token.permissions.includes(scope));
+  }
+}
