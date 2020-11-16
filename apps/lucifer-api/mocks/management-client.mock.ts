d67adc5ea4fe228cc74578cfc1ac847b3e2996d2
Julien Capellari <julien.capellari@neoxia.com>
2020-11-16 14:06:37 +0100

    update(lucifer-api): test users service list

--- apps/lucifer-api/mocks/management-client.mock.ts
+++ apps/lucifer-api/mocks/management-client.mock.ts
@@ -3,7 +3,11 @@ import { AppMetadata, ObjectWithId, User, UserMetadata } from 'auth0';
 // Mock
 export class ManagementClientMock<A = AppMetadata, U = UserMetadata> {
   // Methods
-  getUser = async (params: ObjectWithId): Promise<User<A, U> | undefined> => {
+  async getUser(params: ObjectWithId): Promise<User<A, U> | null> {
     return { user_id: params.id };
-  };
+  }
+
+  async getUsers(): Promise<User<A, U>[]> {
+    return [];
+  }
 }
--- apps/lucifer-api/src/users/users.test.ts
+++ apps/lucifer-api/src/users/users.test.ts
@@ -16,7 +16,7 @@ beforeAll(async () => {
   app = await Test.createTestingModule({
     imports: [UsersModule]
   })
-    .overrideProvider(ManagementClient).useValue(new ManagementClientMock())
+    .overrideProvider(ManagementClient).useClass(ManagementClientMock)
     .compile();
 
   service = app.get(UsersService);
@@ -67,10 +67,60 @@ describe('UsersService.get', () => {
   it('should throw if user is undefined', async () => {
     // Mock
     jest.spyOn(mgmtClient, 'getUser')
-      .mockImplementation(async () => undefined);
+      .mockImplementation(async () => null);
 
     // Call
     await expect(service.get(user.user_id))
       .rejects.toEqual(new NotFoundException(`User ${user.user_id} not found`));
   });
 })
+
+describe('UsersService.list', () => {
+  const users = [
+    {
+      user_id:  'tests|users-auth0-21',
+      email:    'test21@users.auth0',
+      emailVerified: false,
+      name:     'Test',
+      nickname: 'test',
+      picture:  'https://auth0.users.com/test21'
+    },
+    {
+      user_id:  'tests|users-auth0-22',
+      email:    'test22@users.auth0',
+      emailVerified: false,
+      name:     'Test',
+      nickname: 'test',
+      picture:  'https://auth0.users.com/test22'
+    }
+  ];
+
+  // Tests
+  it('should return parsed user list', async () => {
+    // Mock
+    const spy = jest.spyOn(mgmtClient, 'getUsers')
+      .mockImplementation(async () => users);
+
+    // Call
+    await expect(service.list())
+      .resolves.toEqual(
+        users.map(usr => expect.objectContaining({ id: usr.user_id }))
+      );
+
+    // Check call
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith({ sort: 'user_id:1' });
+  });
+
+  it('should throw if auth0 throws', async () => {
+    // Mock
+    const error = new Error();
+
+    jest.spyOn(mgmtClient, 'getUsers')
+      .mockImplementation(async () => { throw error; });
+
+    // Call
+    await expect(service.list())
+      .rejects.toBe(error);
+  });
+});
