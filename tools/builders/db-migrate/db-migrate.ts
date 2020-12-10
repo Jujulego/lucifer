7b946cf736222d0cf6feb0654be81628f7e9d74e
Julien Capellari <julien.capellari@neoxia.com>
2020-12-10 16:50:32 +0100

    update(tools): generic spawn typeorm

---
+++ tools/builders/db-migrate/db-migrate.ts
@@ -0,0 +1,18 @@
+import { BuilderContext, createBuilder } from '@angular-devkit/architect';
+import { json } from '@angular-devkit/core';
+
+import { spawnTypeorm } from '../typeorm';
+
+// Options
+interface Options extends json.JsonObject {
+  database: string;
+}
+
+// Builder
+export default createBuilder(async (options: Options, ctx: BuilderContext) => {
+  // Migrate database
+  ctx.reportStatus('migrating')
+  ctx.logger.info(`Migrating database ${options.database}`);
+
+  return await spawnTypeorm(ctx, 'migration:run');
+});
---
+++ tools/builders/typeorm.ts
@@ -0,0 +1,28 @@
+import { BuilderContext, BuilderOutput } from '@angular-devkit/architect';
+import { spawn } from 'child_process';
+
+// Type
+export type TypeormCommand = 'migration:run';
+
+// Constants
+const TSNODE_ARGS = ['-P', 'tools/tsconfig.tools.json', '-r', 'tsconfig-paths/register'];
+const TYPEORM_CLI = './node_modules/typeorm/cli.js';
+
+// Utils
+export async function spawnTypeorm(ctx: BuilderContext, cmd: TypeormCommand, ...args: string[]): Promise<BuilderOutput> {
+  try {
+    const child = spawn('ts-node', [...TSNODE_ARGS, TYPEORM_CLI, cmd, ...args], {
+      shell: true,
+      stdio: 'inherit'
+    });
+
+    return new Promise<BuilderOutput>((resolve) => {
+      child.on('close', (code) => {
+        resolve({ success: code === 0 });
+      });
+    });
+  } catch (error) {
+    ctx.logger.error(error);
+    return { success: false };
+  }
+}
