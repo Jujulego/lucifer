641b55b660650ef343b59ee74eb661c4b3aec2f6
Julien Capellari <julien.capellari@neoxia.com>
2020-12-11 17:54:02 +0100

    fix(lucifer-api): tests

--- apps/lucifer-api/src/app.module.ts
+++ apps/lucifer-api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database.module';
 import { UsersModule } from './users/users.module';
 import { TransformInterceptor } from './transform.interceptor';
 
@@ -9,6 +10,7 @@ import { TransformInterceptor } from './transform.interceptor';
 @Module({
   imports: [
     AuthModule,
+    DatabaseModule,
     UsersModule
   ],
   providers: [
