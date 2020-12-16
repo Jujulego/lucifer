bd81e9616f23783fb8ef3785cd3298e260352275
Julien Capellari <julien.capellari@neoxia.com>
2020-12-16 16:19:03 +0100

    update(tools): creates remote

--- tools/executors/deploy/index.ts
+++ tools/executors/deploy/index.ts
@@ -2,23 +2,44 @@ import { BuilderContext, createBuilder } from '@angular-devkit/architect';
 import { json } from '@angular-devkit/core';
 import simpleGit from 'simple-git';
 
-// Executor
-export default createBuilder(async (options: json.JsonObject, ctx: BuilderContext) => {
-  // Setup
-  const git = simpleGit(ctx.workspaceRoot);
+import { spawn } from '../utils';
 
-  if (!ctx.target) {
-    ctx.logger.error("Missing target !");
+// Options
+interface Options extends json.JsonObject {
+  buildPath: string;
+}
+
+// Executor
+export default createBuilder(async (options: Options, ctx: BuilderContext) => {
+  try {
+    // Setup
+    const git = simpleGit(ctx.workspaceRoot);
+
+    if (!ctx.target) {
+      ctx.logger.error("Missing target !");
+      return { success: false };
+    }
+
+    // Get current project
+    const { project } = ctx.target;
+    const remote = `heroku-${project}`;
+
+    // Create remote
+    const hasRemote = (await git.getRemotes()).some(rmt => rmt.name === remote);
+
+    if (!hasRemote) {
+      ctx.logger.info(`Creating remote ${remote} ...`);
+
+      if (!await spawn('heroku', ['git:remote', '-a', project, '-r', remote])) {
+        ctx.logger.error("Failed to add heroku remote !");
+        return { success: false };
+      }
+    }
+
+    return { success: true };
+
+  } catch (error) {
+    ctx.logger.error(error);
     return { success: false };
   }
-
-  // Get current project
-  const { project } = ctx.target;
-  const remote = `heroku-${project}`;
-
-  // Check remotes
-  const hasRemote = (await git.getRemotes()).some(rmt => rmt.name === remote);
-  ctx.logger.info(hasRemote ? 'remote :D' : 'no remote :(');
-
-  return { success: true };
 });
