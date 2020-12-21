e83cb61069a796d6546babea9e069de1b41d771e
Julien Capellari <julien.capellari@neoxia.com>
2020-12-21 15:38:34 +0100

    update(workflow): setup-db I

--- .github/actions/setup-db/index.js
+++ .github/actions/setup-db/index.js
@@ -1,9 +1,12 @@
 const core = require('@actions/core');
+const fse = require('fs-extra');
 const path = require('path');
 
 (async () => {
   try {
-    console.log(process.cwd());
+    const workspace = fse.readJson('workspace.json');
+    console.log(workspace);
+
     core.setFailed('!');
   } catch (error) {
     core.setFailed(error.message);
