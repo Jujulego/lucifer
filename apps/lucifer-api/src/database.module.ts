0d8be36e71bc4158ece0d925d1d9d2f4f52272c9
Julien Capellari <julien@capellari.net>
2020-12-22 23:25:40 +0100

    fix(lucifer-api): tests

--- apps/lucifer-api/src/database.module.ts
+++ apps/lucifer-api/src/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConnectionOptionsReader } from 'typeorm';
+import * as path from 'path';
 
 import { MIGRATIONS } from './db/migrations';
 import { env } from './env';
@@ -16,10 +17,14 @@ import { env } from './env';
         }
 
         if (!env.DATABASE_URL) {
-          const reader = new ConnectionOptionsReader({
-            root: __dirname.replace(/\\dist/, ''),
-          });
+          let root = __dirname;
+          if (root.endsWith('src')) {
+            root = path.dirname(__dirname);
+          } else {
+            root = root.replace(/\\dist/, '');
+          }
 
+          const reader = new ConnectionOptionsReader({ root });
           options = await reader.get('default');
         }
 
---
+++ apps/lucifer-api/src/db/migrations.ts
@@ -0,0 +1,7 @@
+import { LocalUser1607694957476 } from './migrations/1607694957476-LocalUser';
+import { Machine1607900505621 } from './migrations/1607900505621-Machine';
+
+export const MIGRATIONS = [
+  LocalUser1607694957476,
+  Machine1607900505621
+]
