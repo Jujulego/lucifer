ee2a2992e6d320d8c963ed5e29f7f4580fb2a882
Julien Capellari <julien@capellari.net>
2021-01-02 00:46:52 +0100

    update(react-basics): migrate toolbar action stories

--- libs/react/basics/src/lib/ToolbarAction.stories.tsx
+++ libs/react/basics/src/lib/ToolbarAction.stories.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Meta, Story } from '@storybook/react';
+
 import { Send as SendIcon } from '@material-ui/icons';
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
 
-import ToolbarAction from '../lib/ToolbarAction';
+import ToolbarAction, { ToolbarActionProps } from '../lib/ToolbarAction';
 
-// Stories
+// config
 export default {
   title: 'Basics/ToolbarAction',
-  component: ToolbarAction
-}
+  component: ToolbarAction,
+  argTypes: {
+    onClick: { action: 'clicked' }
+  }
+} as Meta;
 
-export const story = () => (
-  <ToolbarAction
-    tooltip={text('tooltip', 'Test')}
-    onClick={action('click')}
-  >
+// Stories
+const Template: Story<ToolbarActionProps> = (args) => (
+  <ToolbarAction {...args}>
     <SendIcon />
   </ToolbarAction>
-)
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  tooltip: 'Test',
+};
