4845764532a41c06115d33fb244a95cc1054ae84
Julien Capellari <julien.capellari@neoxia.com>
2021-01-11 12:59:57 +0100

    fix(res): auth0 domain

--- apps/lucifer-front/src/configs/auth.ts
+++ apps/lucifer-front/src/configs/auth.ts
@@ -2,7 +2,7 @@ import { env } from '../environments/environment';
 
 // Auth config
 const authConfig = {
-  domain: 'dev-lucifer.eu.auth0.com',
+  domain: 'jujulego.eu.auth0.com',
   clientId: env.auth0ClientId,
   audience: 'https://lucifer-api.herokuapp.com/'
 }
