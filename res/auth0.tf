762c97beecb4415a9825381fa16aaad8ed5f3a7c
Julien Capellari <julien.capellari@neoxia.com>
2021-01-12 15:56:04 +0100

    add(res): auth0 rule

--- res/auth0.tf
+++ res/auth0.tf
@@ -1,3 +1,17 @@
 resource "auth0_tenant" "tenant" {
   default_directory = "Username-Password-Authentication"
 }
+
+// Rules
+locals {
+  rules-directory = "${path.root}/auth0-rules"
+}
+
+data "local_file" "add-permissions" {
+  filename = "${local.rules-directory}/add-permissions.js"
+}
+
+resource "auth0_rule" "add-permissions" {
+  name   = "Add Permissions"
+  script = data.local_file.add-permissions.content
+}
