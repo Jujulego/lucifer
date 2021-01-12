84b369a6c090790624f727247a0031a7650b2d1b
Julien Capellari <julien.capellari@neoxia.com>
2021-01-12 16:21:37 +0100

    update(workflow): prepare rules for terraform

--- res/auth0.tf
+++ res/auth0.tf
@@ -3,12 +3,8 @@ resource "auth0_tenant" "tenant" {
 }
 
 // Rules
-locals {
-  rules-directory = "${path.root}/auth0-rules"
-}
-
 resource "auth0_rule" "rule" {
-  for_each = fileset(path.root, "auth0-rules/*.js")
+  for_each = fileset(path.root, "rules/*.js")
 
   name   = title(replace(trimsuffix(basename(each.value), ".js"), "-", " "))
   script = file(each.value)
