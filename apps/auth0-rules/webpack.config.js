114f57cb75fbf8fc313527ad087f005cc52d2fa5
Julien Capellari <julien.capellari@neoxia.com>
2021-01-12 15:06:24 +0100

    add(auth0-rules): add-permissions rule

--- apps/auth0-rules/webpack.config.js
+++ apps/auth0-rules/webpack.config.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 /**
- * Add all .ts files under the source root as entrypoints
+ * Add all .ts files under the "rules" directory as entrypoints
  * @param {Required<import('webpack').webpack.Configuration>} config
  * @param context
  * @return {import('webpack').webpack.Configuration}
@@ -11,12 +11,12 @@ module.exports = (config, context) => {
   const { root, sourceRoot } = context.options;
 
   // Build entrypoints
-  config.entry = () => fs.readdir(sourceRoot)
+  config.entry = () => fs.readdir(path.join(sourceRoot, 'rules'))
     .then((rules) => rules
       .filter((rule) => rule.endsWith('.ts'))
       .reduce((entries, rule) => {
         const name = rule.split('.')[0];
-        entries[name] = path.join(root, sourceRoot, rule);
+        entries[name] = path.join(root, sourceRoot, 'rules', rule);
 
         return entries;
       }, {})
