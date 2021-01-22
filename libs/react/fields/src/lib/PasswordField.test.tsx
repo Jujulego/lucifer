9850fee5f30f262c3794017d79db10b6329bd13e
Julien Capellari <julien.capellari@neoxia.com>
2021-01-22 17:31:54 +0100

    add(react-basics): test PasswordField

---
+++ libs/react/fields/src/lib/PasswordField.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import PasswordField from './PasswordField';
+
+// Test suites
+describe('PasswordField', () => {
+  it('should change input type on button click', async () => {
+    // Render
+    render(<PasswordField value="password" />);
+
+    // Get elements
+    const button = screen.getByRole('button');
+    const input = screen.getByDisplayValue('password');
+
+    // Begin with type password
+    expect(input.getAttribute('type')).toBe('password');
+
+    // 1st click => change to text
+    userEvent.click(button);
+    expect(input.getAttribute('type')).toBe('text');
+
+    // 2nd click => back to password
+    userEvent.click(button);
+    expect(input.getAttribute('type')).toBe('password');
+  });
+})
