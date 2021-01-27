7a5adc7f5f3668f170c01dddaa93903551b9e60e
Julien Capellari <julien.capellari@neoxia.com>
2021-01-27 17:02:42 +0100

    update(react-api): add tests

--- libs/react/api/src/lib/api.test.tsx
+++ libs/react/api/src/lib/api.test.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import axios from 'axios';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
