1ffd5c9413d053e499b21171080a4f31674038fa
Julien Capellari <julien.capellari@neoxia.com>
2021-01-28 18:00:50 +0100

    update(auth0): auth0 front client config

--- res/lucifer-front.tf
+++ res/lucifer-front.tf
@@ -2,9 +2,14 @@ resource "auth0_client" "lucifer-front" {
   name                = "Lucifer"
   app_type            = "spa"
   grant_types         = ["implicit", "authorization_code", "refresh_token"]
+  oidc_conformant     = true
   callbacks           = ["http://localhost:4200", heroku_app.lucifer-front.web_url]
   allowed_logout_urls = ["http://localhost:4200", heroku_app.lucifer-front.web_url]
   web_origins         = ["http://localhost:4200", heroku_app.lucifer-front.web_url]
+
+  jwt_configuration {
+    alg = "RS256"
+  }
 }
 
 resource "heroku_app" "lucifer-front" {
