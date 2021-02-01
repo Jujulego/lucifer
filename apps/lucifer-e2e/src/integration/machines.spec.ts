5d3b40bcfcf86beace359187275176e0858fbbdb
Julien Capellari <julien.capellari@neoxia.com>
2021-02-01 16:56:59 +0100

    update(lucifer-e2e): test if has seeded machines

--- apps/lucifer-e2e/src/integration/machines.spec.ts
+++ apps/lucifer-e2e/src/integration/machines.spec.ts
@@ -1,4 +1,4 @@
-import { getMachineTable } from '../support/machines.po';
+import { getLoader, getMachineTable } from '../support/machines.po';
 
 before(() => {
   cy.login();
@@ -10,6 +10,12 @@ describe('Machines table', () => {
   });
 
   it('should show machine table', () => {
+    getLoader().should('not.exist');
+
     getMachineTable().should('exist');
+    getMachineTable().findAllByRole('row').should('have.length', '4');
+    getMachineTable().findByText('Test #1').should('exist');
+    getMachineTable().findByText('Test #2').should('exist');
+    getMachineTable().findByText('Test #3').should('exist');
   });
 });
--- apps/lucifer-e2e/src/support/machines.po.ts
+++ apps/lucifer-e2e/src/support/machines.po.ts
@@ -1,3 +1,4 @@
 export const getMain = () => cy.findByRole('main');
 
+export const getLoader = () => getMain().findByRole('progressbar');
 export const getMachineTable = () => getMain().findByRole('table');
