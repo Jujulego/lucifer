9d8d8298ae0e261a1d0f0cf5ee77d5f44c9e5f22
Julien Capellari <julien@capellari.net>
2021-02-02 22:18:00 +0100

    update(lucifer-api): remove machine migrations

--- apps/lucifer-api/src/db/migrations.ts
+++ apps/lucifer-api/src/db/migrations.ts
@@ -1,13 +1,9 @@
 import { LocalUser1607694957476 } from './migrations/1607694957476-LocalUser';
-import { Machine1607900505621 } from './migrations/1607900505621-Machine';
 import { Project1609715201729 } from './migrations/1609715201729-Project';
 import { Variable1611440573939 } from './migrations/1611440573939-Variable';
-import { RemoveMachine1612281551666 } from './migrations/1612281551666-RemoveMachine';
 
 export const MIGRATIONS = [
   LocalUser1607694957476,
-  Machine1607900505621,
   Project1609715201729,
-  Variable1611440573939,
-  RemoveMachine1612281551666
+  Variable1611440573939
 ];
