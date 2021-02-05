95f931d54bee5261b5c5801864934e08be157da6
Julien Capellari <julien@capellari.net>
2021-02-06 00:58:41 +0100

    update(lucifer-api): setup backend code coverage

---
+++ apps/lucifer-api/webpack-e2e.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = (config, context) => {
+  const options = context.buildOptions || context.options;
+
+  config.module.rules.push({
+    test: /\.(js|ts)x?$/,
+    loader: 'istanbul-instrumenter-loader',
+    options: { esModules: true },
+    enforce: 'post',
+    include: [
+      path.join(__dirname, '..', 'src'),
+      path.join(options.root, 'libs'),
+    ],
+    exclude: [
+      /\.(e2e|test)\.ts$/,
+      /node_modules/
+    ]
+  });
+
+  return config;
+};
