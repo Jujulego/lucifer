b17ff672fd58cd5ee31dfee44cca0f6078354475
Julien Capellari <julien.capellari@gmail.com>
2021-02-18 18:11:06 +0100

    fix(res): wrong cli grant type

--- res/lucifer-cli.tf
+++ res/lucifer-cli.tf
@@ -2,7 +2,7 @@
 resource "auth0_client" "lucifer-cli" {
   name            = "Lucifer CLI"
   app_type        = "native"
-  grant_types     = ["device_code", "refresh_token"]
+  grant_types     = ["urn:ietf:params:oauth:grant-type:device_code", "refresh_token"]
   oidc_conformant = true
 
   jwt_configuration {
