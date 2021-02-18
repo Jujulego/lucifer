aa2c85c88001838934b4a971b49757704963ac1f
Julien Capellari <julien.capellari@gmail.com>
2021-02-18 18:35:13 +0100

    fix(res): lucifer-cli

--- res/lucifer-cli.tf
+++ res/lucifer-cli.tf
@@ -1,9 +1,10 @@
 # Auth0 resources
 resource "auth0_client" "lucifer-cli" {
-  name            = "Lucifer CLI"
-  app_type        = "native"
-  grant_types     = ["urn:ietf:params:oauth:grant-type:device_code", "refresh_token"]
-  oidc_conformant = true
+  name                       = "Lucifer CLI"
+  app_type                   = "native"
+  grant_types                = ["urn:ietf:params:oauth:grant-type:device_code", "refresh_token"]
+  oidc_conformant            = true
+  token_endpoint_auth_method = "none"
 
   jwt_configuration {
     alg = "RS256"
