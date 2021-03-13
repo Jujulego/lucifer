478d856abbef1218948983103d32d6731fb1a404
Julien Capellari <julien.capellari@gmail.com>
2021-03-13 12:13:47 +0100

    fix(workflow): test & build errors

---
+++ libs/types/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  displayName: 'types',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/types',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!**/*.test.ts'
+  ]
+};
---
+++ libs/utils/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  displayName: 'utils',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/utils',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!**/*.test.ts'
+  ]
+};
